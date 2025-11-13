1b05cd4a01798d8862edd9ffa9624bae94498890
Julian Rakar <julian.r42@gso.schule.koeln>
2025-11-13 21:05:26 +0100

    PHP white screen of death fix 2 Debug code adedd

--- web/index.php
+++ web/index.php
@@ -1,4 +1,9 @@
 <?php
+//debug code hilfe
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // === Verbindung zur Datenbank ===
 $host = "db";     
 $benutzer = "root";      
